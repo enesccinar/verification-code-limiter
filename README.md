9ec29b7d9902e119680981e93bbab80e74c9b66b
enesccinar <enescihancinar@gmail.com>
2021-02-27 22:20:36 +0000

    cron job added

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # verification-code-limiter
 
+This package stores the given phone numbers in a hash table. A Cron job reset the table every 24 hours.
+
 GitHub Project:
 https://github.com/enesccinar/verification-code-limiter
 
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const HashTable = require('./src/hashTable')
+const cron = require('node-cron')
 
-const ht = new HashTable()
+let ht = new HashTable()
 
-exports.check = function(phoneNumber) {
+exports.check = function (phoneNumber) {
     return ht.add(phoneNumber);
 }
+
+cron.schedule('* 12 * * *', () => {
+    console.info('Create new verification code table code executed!')
+    ht = new HashTable()
+})
